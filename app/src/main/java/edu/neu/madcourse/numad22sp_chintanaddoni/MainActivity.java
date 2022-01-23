75ab95d6b40aa26a6fc6490c0a4e9b504a06c40c
chintansa <chintansa148@gmail.com.com>
2022-01-23 15:08:35 -0500

    inital App push

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_chintanaddoni/MainActivity.java
@@ -0,0 +1,22 @@
+package edu.neu.madcourse.numad22sp_chintanaddoni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(v -> Toast.makeText(getApplicationContext(),
+                "Name: Chintan Addoni \nEmail: sheshagiriaddoni.c@northeastern.edu",
+                Toast.LENGTH_LONG)
+                .show());
+    }
+}
