3b05a4c4f8bdb5ac86be18af065c03210564f859
Chintan S A <chintansa148@gmail.com>
2022-02-06 14:24:58 -0500

    Updated the about page

--- app/src/main/java/edu/neu/madcourse/numad22sp_chintanaddoni/FirstFragment.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_chintanaddoni/FirstFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -30,11 +31,11 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                Toast.makeText(getActivity().getApplicationContext(), "Name: Chintan Addoni\n" +
+                        "Email: sheshagiriaddoni.c@northeastern.edu", Toast.LENGTH_LONG).show();
             }
         });
 
